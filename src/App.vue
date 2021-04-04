48b0da6fcec681bbab5768c8f32a09b40fdf3e7d
kafribung <60900117001@uin-alauddin.ac.id>
2021-04-04 21:58:08 +0800

    install tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-      <div class="text-sm text-red-400 font-extrabold">Kafri</div>
+      <div class="text-sm font-extrabold">Kafri</div>
       <div class="bg-purple text-white sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange ...">
         Test
       </div>
