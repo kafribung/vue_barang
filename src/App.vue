799acc15daf458657fc01a2614a7353ac9b18b63
kafribung <60900117001@uin-alauddin.ac.id>
2021-04-05 23:58:35 +0800

    word spacing

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
       <div class="text-sm font-extrabold">Kafri</div>
-       <div class="bg-purple text-white sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange ...">
-         Kafri
+      <div class="bg-purple text-white sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange ...">
+          Kafri
       </div>
   </div>
 </template>
