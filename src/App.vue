77ecccd9f1bcd3308b2313174ea3e0aeb9639df6
kafribung <60900117001@uin-alauddin.ac.id>
2021-04-05 23:57:04 +0800

    mantap

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
       <div class="text-sm font-extrabold">Kafri</div>
-      <div class="bg-purple text-white sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange ...">
-        
-      </div>
   </div>
 </template>
 
