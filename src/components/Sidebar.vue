0390f16fd463e4c4f3930fb4a9847dedf8d24d29
kafribung <60900117001@uin-alauddin.ac.id>
2021-04-13 22:47:37 +0800

    bug hamburger

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -109,11 +109,13 @@
         </aside>
         <!-- END Desktop sidebar -->
 
+        <h1> Sapi{{ toggleSideMenu }}</h1>
+
         <!-- Mobile sidebar -->
         <!-- Backdrop -->
-        <div  v-show="toggleSideMenuMobile" x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center" > </div>
+        <div  v-show="toggleSideMenu" x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center" > </div>
         <!-- END Backdrop -->
-        <aside class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden" v-show="toggleSideMenuMobile"  x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0 transform -translate-x-20" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0 transform -translate-x-20">
+        <aside class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden" v-show="toggleSideMenu"  x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0 transform -translate-x-20" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0 transform -translate-x-20">
             <div class="py-4 text-gray-500 dark:text-gray-400">
             <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#" >Windmill</a>
             <ul class="mt-6">
@@ -229,7 +231,16 @@ export default {
             // Side Bar
             togglePagesMenu: false,
             closeSideMenu: false,
-            toggleSideMenuMobile: false,
+            toggleSideMenu: 'ok',
+        }
+    },
+    created() {
+        this.getToggleSideMenu
+    },
+    methods: {
+        getToggleSideMenu(){
+            let data = localStorage.getItem('toggleSideMenuMobile')
+            console.log(data)
         }
     },
 };
