ae7062f8f24de22bcd50e7148b3a1e27a9aab703
kafribung <60900117001@uin-alauddin.ac.id>
2021-04-18 23:03:38 +0800

    clean table

--- src/Dashboard.vue
+++ src/Dashboard.vue
@@ -399,18 +399,14 @@
               <div class="w-full overflow-x-auto">
                 <table class="w-full whitespace-no-wrap">
                   <thead>
-                    <tr
-                      class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
-                    >
+                    <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800" >
                       <th class="px-4 py-3">Client</th>
                       <th class="px-4 py-3">Amount</th>
                       <th class="px-4 py-3">Status</th>
                       <th class="px-4 py-3">Date</th>
                     </tr>
                   </thead>
-                  <tbody
-                    class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
-                  >
+                  <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800" >
                     <tr class="text-gray-700 dark:text-gray-400">
                       <td class="px-4 py-3">
                         <div class="flex items-center text-sm">
