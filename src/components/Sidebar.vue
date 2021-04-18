63994b3b1c18dbf91c65e4d1e2b338551f466742
kafribung <60900117001@uin-alauddin.ac.id>
2021-04-18 22:55:59 +0800

    dashboard

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -109,8 +109,6 @@
         </aside>
         <!-- END Desktop sidebar -->
 
-        <h1> Data : {{ toggleSideMenu }}</h1>
-
         <!-- Mobile sidebar -->
         <!-- Backdrop -->
         <div  v-show="toggleSideMenu" x-transition:enter="transition ease-in-out duration-150" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center" > </div>
@@ -234,11 +232,5 @@ export default {
             toggleSideMenu: '',
         }
     },
-    computed:{
-        getToggleSideMenuMobile(){
-            let test=  localStorage.getItem('toggleSideMenuMobile')
-            return this.toggleSideMenu = test
-        }
-    }
 };
 </script>
