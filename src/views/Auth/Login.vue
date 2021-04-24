e52c5df2d00f0f559d197305f4bc11cb04bbf0ad
kafribung <60900117001@uin-alauddin.ac.id>
2021-04-24 16:47:15 +0800

    login bug view

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -16,9 +16,10 @@
                         <label class="block text-sm">
                         <span class="text-gray-700 dark:text-gray-400">Email</span>
                         <input
-                            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                            class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none  focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray"
                             placeholder="Jane Doe" />
                         </label>
+                        <input type="text" placeholder="kafri" class="text-red-600 border-black">
                         <label class="block mt-4 text-sm">
                         <span class="text-gray-700 dark:text-gray-400">Password</span>
                         <input
