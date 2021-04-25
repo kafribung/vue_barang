e306cb2ab9f5928aa6a5e0d2a4d0c0afd7611707
kafribung <60900117001@uin-alauddin.ac.id>
2021-04-25 23:13:57 +0800

    img

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -5,14 +5,13 @@
                 <div class="h-32 md:h-auto md:w-1/2">
                     <img aria-hidden="true" class="object-cover w-full h-full dark:hidden" src="../../assets/img/login-office.jpeg"
                         alt="Office" />
-                    <img aria-hidden="true" class="hidden object-cover w-full h-full dark:block"
-                        src="../../assets/img/login-office-dark.jpeg" alt="Office" />
+                    <img aria-hidden="true" class="hidden object-cover w-full h-full dark:block" src="../../assets/img/login-office-dark.jpeg" alt="Office" />
                     </div>
                     <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
                     <div class="w-full">
                         <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">Login</h1>
                         <label class="block text-sm">
-                        <!-- <span class="text-gray-700 dark:text-gray-400">Email</span> -->
+                        <span class="text-gray-700 dark:text-gray-400">Email</span>
                         <input  class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none bg-gray-100 py-2 rounded-md focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray" placeholder="Jane Doe" />
                         </label>
                         <label class="block mt-4 text-sm">
