21d52d2c2b306d1fdf9f3fbbe49e576be4b16da9
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-01 23:08:15 +0800

    send to attemp method

--- src/store/auth.js
+++ src/store/auth.js
@@ -5,17 +5,21 @@ export default {
         user: null,
     },
     mutations: {
+        SET_TOKEN(state, token) {
+            state.token = token
+        }
     },
     actions: {
-        async login(commit, data) {
+        async login({ dispatch }, data) {
             try {
                 const response = await axios.post('http://127.0.0.1:8000/api/login', data);
-                // this.user  = response.data.name
-                // this.token = response.data.token
-                console.log(response.data.token);
+                // Mengirim daata ke method attemp
+                dispatch('attemp', response.data.token)
             } catch (error) {
                 console.log(error.response.data.msg)
             }
+        },
+        attemp() {
             
         }
     },
