1e8709ef2cee424d70f5a2bfcfe30000db1555ae
kafribung <60900117001@uin-alauddin.ac.id>
2021-05-01 23:19:34 +0800

    cara 2

--- src/store/auth.js
+++ src/store/auth.js
@@ -5,22 +5,26 @@ export default {
         user: null,
     },
     mutations: {
+        SET_NAME(state, name) {
+            state.user = name  
+        },
         SET_TOKEN(state, token) {
             state.token = token
         }
     },
     actions: {
-        async login({ dispatch }, data) {
+        async login({ commit, dispatch }, data) {
             try {
                 const response = await axios.post('http://127.0.0.1:8000/api/login', data);
+
                 // Mengirim daata ke method attemp
-                dispatch('attemp', response.data.token)
+                dispatch('attemp', response.data.data.token)
             } catch (error) {
-                console.log(error.response.data.msg)
+                console.log(error)
             }
         },
-        attemp() {
-            
+        attemp({commit}, token) {
+            commit('SET_TOKEN', token)
         }
     },
 }
