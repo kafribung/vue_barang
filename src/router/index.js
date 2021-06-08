9d003840c84436e1f5229d372aed2e4f1be5563b
=Kafriansyah <60900117001@uin-alauddin.ac.id>
2021-06-08 17:30:09 +0800

    api

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 import Inventory from '../views/Inventory.vue'
-// import axios from 'axios'
+import axios from 'axios'
 
 Vue.use(VueRouter)
 
@@ -15,16 +15,48 @@ const routes = [
     path: '/dashboard',
     name: 'Dashboard',
     component: Dashboard,
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem('token')
+      const accessToken = {
+          headers: {
+              'Authorization': `Bearer ${token}`
+          }
+      }
+      axios.get('127.0.0.1/api/inventory', accessToken)
+        .then((response) => { 
+          console.log(response);
+        })
+      .catch(() => next('/login'))
+    }
+
   },
   {
     path: 'inventory',
     name: 'Inventory',
-    component: Inventory
+    component: Inventory, 
+    beforeEnter: (to, from, next) => {
+      const token = localStorage.getItem('token')
+      const accessToken = {
+          headers: {
+              'Authorization': `Bearer ${token}`
+          }
+      }
+      axios.get('/api/inventory', accessToken)
+        .then(() => { 
+          next()
+        }).
+        catch(() => next('/login'))
+    }
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Auth/Login.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Auth/Login.vue'),
+    // beforeEach: (to, from, next) => {
+    //   if (localStorage.getItem('token') == '') {
+    //       next()
+    //   } else next('/dashboard')
+    // }
   }
 ]
 
